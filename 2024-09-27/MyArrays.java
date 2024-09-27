8c51cc79af04397a0ce1952091100267ca6c3984
Maxim Argat <maximargat@gmail.com>
2024-09-27 10:11:21 -0400

    more test cases, it works

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,13 +9,39 @@ public class MyArrays
         System.out.println(arrayToString(returnCopy(tobecopied)));
         System.out.println("Same addresses?: " + (tobecopied == returnCopy(tobecopied)));
 
+        tobecopied = new int[]{-2147483648, 2147483647, 0, -9289828, 888888888};
+        System.out.println(arrayToString(tobecopied));
+        System.out.println(arrayToString(returnCopy(tobecopied)));
+        System.out.println("Same addresses?: " + (tobecopied == returnCopy(tobecopied)));
+
+        tobecopied = new int[]{};
+        System.out.println(arrayToString(tobecopied));
+        System.out.println(arrayToString(returnCopy(tobecopied)));
+        System.out.println("Same addresses?: " + (tobecopied == returnCopy(tobecopied)));
+
         System.out.println("concatArray(): ");
 
         int[] arr1 = {2, 5, 1, 5, 5, 9};
         int[] arr2 = {7, 4, 7, 4};
-        System.out.println(arrayToString(arr1));
-        System.out.println(arrayToString(arr2));
+        System.out.println(arrayToString(arr1) + " + " + arrayToString(arr2) + " --> ");
         System.out.println(arrayToString(concatArray(arr1, arr2)));
+
+        arr1 = new int[]{};
+        arr2 = new int[]{};
+        System.out.println(arrayToString(arr1) + " + " + arrayToString(arr2) + " --> ");
+        System.out.println(arrayToString(concatArray(arr1, arr2)));
+
+        arr1 = new int[]{-9229892, 9992, 777777};
+        arr2 = new int[]{};
+        System.out.println(arrayToString(arr1) + " + " + arrayToString(arr2) + " --> ");
+        System.out.println(arrayToString(concatArray(arr1, arr2)));
+
+
+        arr1 = new int[]{};
+        arr2 = new int[]{9229892, 9992, 777777};
+        System.out.println(arrayToString(arr1) + " + " + arrayToString(arr2) + " --> ");
+        System.out.println(arrayToString(concatArray(arr1, arr2)));
+
     }
     public static int[] returnCopy(int[] nums)
     {
