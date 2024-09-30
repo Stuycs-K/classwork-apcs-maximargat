cf40676429abbb5e9c9707af88de1b041d9301f3
Maxim Argat <maximargat@gmail.com>
2024-09-30 10:09:06 -0400

    added 2d array sum method and printed out a sum

--- 2024-09-30-2DArray/ArrayMethods.java
+++ 2024-09-30-2DArray/ArrayMethods.java
@@ -4,8 +4,10 @@ public class ArrayMethods
 
     public static void main(String[] args)
     {
-        int[][] array1 = new int[3][4];
+        int[][] array1 = new int[][]{{2, 7, 9}, {3, 6, 1}, {7, 4, 2}}; // sum should be 41
         System.out.println(arrayToString(array1));
+
+        System.out.println(arr2DSum(array1));
     }
 
     public static String arrayToString(int[] array)
@@ -30,5 +32,19 @@ public class ArrayMethods
         if(array.length > 0)
             return s.substring(0, s.length() - 2) + "]";
         return s + "]";
+    }
+
+    public static int arr2DSum(int[][] array)
+    {
+        int sum = 0;
+        for(int i = 0; i < array.length; i++)
+        {
+            int[] subarray = array[i];
+            for(int j = 0; j < subarray.length; j++)
+            {
+                sum += subarray[j];
+            }
+        }
+        return sum;
     }
 }
