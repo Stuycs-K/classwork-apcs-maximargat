a1a06f2d7faa20337d670a0dd0530f8fb5f512bb
Maxim Argat <maximargat@gmail.com>
2024-10-15 09:53:52 -0400

    Added static distance method in Demo.java

--- 2024-10-15-Point/Demo.java
+++ 2024-10-15-Point/Demo.java
@@ -6,7 +6,7 @@ public class Demo
     */
     public static double distance(Point a, Point b)
     {
-        return 0.0;
+        return Math.sqrt((a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY()));
     }
 
     public static void main(String[]args)
@@ -14,8 +14,11 @@ public class Demo
         Point p1 = new Point(1,1);
         Point p2 = new Point(-1,-1);
         Point p3 = new Point(3,4);
-        System.out.println(p3);
-        System.out.println(distance(p1,p2));
+        System.out.println("Point 3: " + p3);
+        Point p3copy = new Point (p3);
+        System.out.println("Point 3 copy: " + p3copy);
+        System.out.println("Same address? " + (p3 == p3copy));
+        System.out.println("Distance from Points 1 and 2: " + distance(p1,p2));
         System.out.println(Point.distance(p1,p2));
         System.out.println(p1.distanceTo(p2));
     }
