80403d5dd7717ff5385ef7807a6cbf8130a01678
Maxim Argat <maximargat@gmail.com>
2024-10-31 09:48:32 -0400

    Reads doubles, adds them up, seems to have a correct result

--- 10-31-Advent/Scanny.java
+++ 10-31-Advent/Scanny.java
@@ -6,10 +6,17 @@ public class Scanny
     static String whatever = "According to all known laws of aviation, there is no way a bee should be able to fly.";
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(whatever);
+        /*Scanner sc = new Scanner(whatever);
         while(sc.hasNext())
         {
             System.out.println(sc.next());
+        }*/
+        Scanner sc2 = new Scanner("10.0 1.04 99.0 -3.0 4.2"); // should be around 111
+        double sum = 0;
+        while(sc2.hasNextDouble())
+        {
+            sum += sc2.nextDouble();
         }
+        System.out.println(sum);
     }
 }
