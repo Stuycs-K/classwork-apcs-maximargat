90a1813644aef6963cee5f8c2f9025937d281882
Maxim Argat <maximargat@gmail.com>
2024-11-04 09:45:19 -0500

    CountTrianglesA works for the provided input file (small one)

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -1,9 +1,34 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class TriangleTester
 {
     public static void main(String[] args)
     {
-        System.out.println(test(10, 12, 14)); // good
-        System.out.println(test(10, 30, 14)); // impossible
+        //System.out.println(test(10, 12, 14)); // good
+        //System.out.println(test(10, 30, 14)); // impossible
+
+        System.out.println(countTrianglesA("inputTri.txt"));
+    }
+    public static int countTrianglesA(String filename)
+    {
+        try
+        {
+            Scanner sc = new Scanner(new File(filename));
+            int countvalid = 0;
+            while(sc.hasNextLine())
+            {
+                if(test(sc.nextInt(), sc.nextInt(), sc.nextInt()))
+                    countvalid++;
+            }
+            return countvalid;
+        }
+        catch(Exception e)
+        {
+            System.out.println("File not found");
+            return -999;
+        }
     }
 
     public static boolean test(int sidea, int sideb, int sidec)
