99adabca505a73775712047f9dfd1805cf0fd826
Maxim Argat <maximargat@gmail.com>
2024-12-09 23:08:32 -0500

    Added / tested input parsing, works

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -4,7 +4,7 @@ public class Game
     static String[] names = {"Ted", "Bill", "Woods", "Joe", "Benneth", "Samuel", "Axel", "Leo", "Casio Calculator", "TI-83 Plus Graphing Calculator"};
     public static void main (String[] args)
     {
-        Scanner input = new Scanner(System.in);
+        Scanner inputsc = new Scanner(System.in);
 
         int firstname = (int)(Math.random() * names.length);
         int secondname = (int)(Math.random() * names.length);
@@ -15,12 +15,44 @@ public class Game
         Thief myguy = new Thief(names[firstname]);
         CodeWarrior otherguy = new CodeWarrior(names[secondname]);
 
-        /*
-        while(false)
+        
+        while(myguy.getHP() > 0 && otherguy.getHP() > 0)
         {
-            
+            // run turn
+            /*
+            Prompt the user: "Type: (a)ttack / (sp)ecial / (su)pport" / quit
+            The user can type a/attack to select attack, or type sp/special to do a special attack, or su/support to support themselves. You then make the character use the appropriate action.
+            If the user types quit, end the program.
+            If an invalid response is typed, ask again until the response is valid.
+            Print the results of the action.
+            Now if the opponent is still alive (HP > 0) have the computer randomly choose to have the opponent attack/special attack the player, or buff themselves.
+            Print the results of the action
+            */
+
+            //print the names / HP / SpecialResource(quantity+name) of both the player and enemy. e.g. Conan, 5/16 HP, 19/20 Rage
+            System.out.println(info(myguy) + "\n" + info(otherguy));
+
+            String input = inputsc.nextLine();
+
+            if(input.equals("a") | input.equals("attack"))
+            {
+                System.out.println("attack");
+            }
+            if(input.equals("sp") | input.equals("special"))
+            {
+                System.out.println("special");
+            }
+            if(input.equals("su") | input.equals("support"))
+            {
+                System.out.println("support");
+            }
+            if(input.equals("quit"))
+            {
+                break;
+            }
+
         }
-        */
+        
 
         // print results
         System.out.println(info(myguy) + "\n" + info(otherguy));
