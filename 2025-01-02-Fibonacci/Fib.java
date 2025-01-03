22c798546132b6be7b51cdf0f0876aa14ba820b4
Maxim Argat <maximargat@gmail.com>
2025-01-03 10:04:16 -0500

    makeWords problem coded and works

--- 2025-01-02-Fibonacci/Fib.java
+++ 2025-01-02-Fibonacci/Fib.java
@@ -3,8 +3,9 @@ public class Fib
     // fib(47): 6.43s, fib(48) is >10s, fib(46) is the last before overflow
     public static void main(String[] args)
     {
-        int num = Integer.parseInt(args[0]);
-        System.out.println("Fib of " + num + ": " + fib(num));
+        //int num = Integer.parseInt(args[0]);
+        //System.out.println("Fib of " + num + ": " + fib(num));
+        makeWords(3, "", "abcd");
     }
     public static int fib(int n)
     {
@@ -12,4 +13,18 @@ public class Fib
             return n;
         return fib(n-1) + fib(n-2);
     }
+
+    public static void makeWords(int remainingLetters, String result, String alphabet)
+    {
+        if(remainingLetters == 0)
+            System.out.println(result);
+        else
+        {
+            for(int i = 0; i < alphabet.length(); i++)
+            {
+                makeWords(remainingLetters - 1, result + alphabet.charAt(i), alphabet);
+            }
+        }
+    }
+
 }
